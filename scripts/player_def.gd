c59188c20dd7c8a8496beb79a31cc47ecb644dfd
Linus2009 <Linus@sesastr.de>
2025-11-14 13:43:10 +0100

    zwischenspeicherung 14.11.2025

--- scripts/player_def.gd
+++ scripts/player_def.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
-@export var speed = 15
+@export var speed = 3
 
-@export var gravity = 75
+@export var mouse_sensitivity = 0.0015 
 
 var velo = Vector3.ZERO
 
@@ -12,12 +12,16 @@ func _physics_process(delta: float) -> void:
 	
 	# In die jeweilige Richtung bewegen
 	if Input.is_action_pressed("move_forwards"):
+		print('nach vorne bewegen')
 		direction.x +=1
 	elif Input.is_action_pressed("move_backwards"):
+		print('nach hinten bewegen')
 		direction.x -=1
 	elif Input.is_action_pressed("move_left"):
+		print('nach links bewegen')
 		direction.z -=1
 	elif Input.is_action_pressed("move_right"):
+		print('nach rechts bewegen')
 		direction.z +=1
 		
 	# damit man nicht diagonal schneller ist
@@ -28,10 +32,11 @@ func _physics_process(delta: float) -> void:
 	velo.x = direction.x * speed
 	velo.z = direction.z * speed
 	
-	# Gravität anwenden 
-	if not is_on_floor():
-		velo.y -= gravity * delta
-	
 	# Spieler bewegen
 	velocity = velo
 	move_and_slide()
+
+#kamera benutzen
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
